67343d47188e475a04943138b8dc000db8f285f7
sabira97 <sbirabbasova1@gmail.com>
2025-09-09 10:42:50 +0400

    project

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/10960@holbertonstudents.com/finalproject-1/.venv"
+set -gx VIRTUAL_ENV "/home/10960@holbertonstudents.com/finalproject-2/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
